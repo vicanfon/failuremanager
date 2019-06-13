ea400db26840c16fccfd3a5017e3466bc89bfc24
Ludo Stellingwerff <ludo@almende.org>
2019-06-13 12:42:05 +0000

    thrusday

--- bin/be/DAL/init.js
+++ bin/be/DAL/init.js
@@ -50,7 +50,7 @@ var getExistingViews = function () {
 module.exports = {
     dbExists: function (cb) {
         storage('GET', '/tables', {}, function (err, response, body) {
-            console.log(response.statusCode);
+            // console.log(response.statusCode);
             if (!err) {
                 if (response.statusCode == 404) {
                     cb(false);
